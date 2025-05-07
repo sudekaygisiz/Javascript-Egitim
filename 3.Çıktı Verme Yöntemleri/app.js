e77ff79e43cfa46eb61eab9a59a0cd0a888ba3f9
SudeKaygisiz <sudekaygisiz22@gmail.com>
2025-05-07 20:16:33 +0300

    3.günün içeriği hazırlandı

---
+++ 3.Çıktı Verme Yöntemleri/app.js
@@ -0,0 +1,30 @@
+//; şart değil ama kullanmaya alışmamız gerekiyor
+
+//Document write 
+//Sayfada direk yer alıyor bu
+document.write("**************<br>");
+document.write("Document write ile ekrana yazdırma işlemi<br>");
+document.write("**************<br>");
+
+//Console.log
+//console üzerinde görünür
+console.log("**************");
+console.log("Console log ile ekrana yazdırma işlemi");
+console.log("**************");
+
+console.clear();//consolu temizliyor hepsini siliyor
+
+//Alert popup ile çıktı verme
+alert("Bu Bir Alert mesajıdır.");
+
+
+/*
+     BETTER COMMENTS EKLENTİSİNİN ÖZELLİĞİ
+     ? SORU İŞARETİ MAVİ YAPIYOR
+     ! ÜNLEM İŞARETİ KIRMIZI YAPIYOR
+     * YILDIZ İŞARETİ AÇIK YEŞİL YAPIYOR
+     TODO TODODA TURUNCU YAPIYOR
+
+     BU ŞEKİLDE YORUM SATIRLARINIZI DAHA ÇARPICI YAPABİLİRSİNİZ
+     "//" İLEDE KULLANABİLİRSİNİZ
+*/
